1d8ad7a40d4399d7e95a1edf5aff24006318d223
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2021-03-05 14:48:58 +0500

    Frontend Authentication

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import './../styles/App.css';
 import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
+import Login from './Login';
 import {Route, Router, Switch} from "react-router-dom";
 
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     </div>
